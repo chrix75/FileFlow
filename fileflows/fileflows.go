e6428919bf154ff628922c848b43314567dc32e3
Christian Sperandio <christian.sperandio@gmail.com>
2023-05-29 10:38:38 +0200

    add doc

--- fileflows/fileflows.go
+++ fileflows/fileflows.go
@@ -119,6 +119,20 @@ func (f *FileFlow) IsRemote() bool {
 	return f.Port > 0
 }
 
+// NewSFTPFileFlow creates a new SFTP file flow.
+// name is the name of the flow.
+// privateKeyPath is the path to the private key file to SFTP connection.
+// sourceFolder is the path to the source folder from where files are downloaded. This path is relative to the SFTP user root folder.
+// pattern is the regular expression used to match files.
+// destinationFolders is the list of destination folders where files are downloaded.
+// operation is the operation to perform on the files: MOVE, COMPRESSION, DECOMPRESSION. Format compression supported is gzip.
+// maxFileCount is the maximum number of files that can be downloaded to a destination folder.
+// overflowFolder is the path to the folder where overflow files are stored.
+//
+// For overflow, it's valid when one destination folder and maxFileCount are specified.
+// By default, when the number of files reaches the maxFileCount, file downloads are stopped.
+// But, if overflowFolder is specified, file are downloaded into the overflow folder.
+// When then number of files in destination is less than maxFileCount, the application moves files from the overflow folder to the destination folder.
 func NewSFTPFileFlow(name string,
 	server string, port int,
 	privateKeyPath string,
@@ -147,6 +161,8 @@ func NewSFTPFileFlow(name string,
 	}
 }
 
+// NewLocalFileFlow creates a new local file flow.
+// See NewSFTPFileFlow for details.
 func NewLocalFileFlow(name, sourceFolder, pattern string,
 	destinations []string,
 	operation FlowOperation,
